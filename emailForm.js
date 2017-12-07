3342328b1c59fecc0ddf5520c4e9080280866705
Lovisa Spångberg <lovisa@Lovisas-MacBook-Air.local>
2017-12-07 20:25:23 +0100

    form validation

--- emailForm.js
+++ emailForm.js
@@ -1,15 +1,60 @@
 $(document).ready(function () {
-    $('#emailForm').on('submit', function(e) {
+    $('#Formsubmit').on('submit', function(e) {
         e.preventDefault();
-        var emailField = document.getElementById('emailField').value;
-        var result = validateEmail(emailField);
-        if (!result) {
-          console.log(document.getElementById('unvalidEpost'))
-          document.getElementById('unvalidEpost').classList.add('show-unvalid');
+        var unvalidElement = document.getElementById('unvalidInput');
+        // var successElement = document.getElementById('successElement');
+        var form = document.forms["memberForm"]
+
+        var firstName = form.firstName.value;
+        var lastName = form.lastName.value;
+        var city = form.city.value;
+        var mail = form.email1.value;
+        var mailConfirm = form.email2.value;
+
+        var isValidEmail = validateEmail(mail);
+        var allFieldsAreFilled = (firstName && lastName && city)
+        var emailIsConfirmed = (mail === mailConfirm)
+
+        console.log('isValidEmail', isValidEmail)
+        console.log('allFieldsAreFilled', allFieldsAreFilled)
+        console.log('emailIsConfirmed', emailIsConfirmed)
+
+        if (!allFieldsAreFilled) {
+            console.log('All fields are not filled!')
+            unvalidElement.innerHTML = "Vänligen fyll i alla fälten";
+            unvalidElement.classList.add('show-unvalid');
+
+        } else if (!isValidEmail) {
+            console.log('Email is not valid!')
+            unvalidElement.innerHTML = "Vänligen fyll i en giltig epostadress";
+            unvalidElement.classList.add('show-unvalid');
+
+        } else if (!emailIsConfirmed) {
+            console.log('Email is not confirmed!')
+            unvalidElement.innerHTML = "Vänligen skriv in samma epostadress i båda fälten";
+            unvalidElement.classList.add('show-unvalid');
+
         } else {
-          window.location.replace("http://stackoverflow.com");
+            console.log('Success!!!! :)')
+            // $("#Formsubmit").submit();
+
+            // var url = "testurl.com/test"
+            // var data = {
+            //   firstName: firstName,
+            //   lastName: lastName,
+            //   city: city,
+            //   mail: mail,
+            // }
+
+            // $.ajax({
+            //   method: "POST",
+            //   url: url,
+            //   data: data
+            // }).done(function(msg) {
+            //   successElement.innerHTML = "Ditt konto är skapat!";
+            //   successElement.classList.add('show-success');
+            // });    
         }
-        console.log(result)
     });
 });
 
